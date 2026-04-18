d54c3da2e9eb2db8039877179039f44bfce8963f
Samkelo Mlotshwa <samkelomlotshwa0414@gmail.com>
2026-04-18 14:02:47 +0200

    changesd made

--- level1.py
+++ level1.py
@@ -2,11 +2,11 @@ import json
 import math
 import sys
 
-from Objects.Track import Track
-from Objects.Tyres import Tyres 
-from Objects.Car import Car
-from Objects.Weather import Weather
-from Objects.Race import Race 
+from src.Objects.Track import Track
+from src.Objects.Tyres import Tyres 
+from src.Objects.Car import Car
+from src.Objects.Weather import Weather
+from src.Objects.Race import Race 
 
 class LevelData:
     def __init__(self,car,race,track,tyres,weather):
@@ -22,6 +22,81 @@ class LevelData:
         track_data= data["track"]
         tyres_data= data["tyres"]
         weather_data= data["weather"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class StraightPlan:
     def __init__(self, target_mps,brake_start):
@@ -49,7 +124,56 @@ class level1:
             laps.append({
                 "lap": lap_number,
                 "segments":lap_segments,
-                "pit":{"enter":false},
+                "pit":{"enter":False},
             })
         
+        return {
+            "initial_tyre_id": self.init_tyre_id,
+            "laps": laps,
+        }
+        
+def choose_init_tyre(self):
+    best_id= None
+    best_compound= None
+    best_friction=-1.0
+
+    for tyre_set in self.level.tyres.available_sets:
+        friction=self.compute_tyre_friction(
+            tyre_set.compound,
+            self.start_weather.condition,
+        )
+        if friction > best_friction:
+            best_friction=friction
+            best_compound=tyre_set.compound
+            best_id= tyre_set.ids[0]
+
+    if best_id is None or best_compound is None:
+        raise ValueError("No tyre sets available")
+    return best_id, best_compound
+def compute_tyre_friction(self, compound, weather_condition):
+    base= Tyres.tyres[compound][0]
+    props= self.level.tyres.properties[compound]
+
+    if weather_condition == "dry":
+        multiplier= props.dry_friction_mult
+    elif weather_condition =="cold":
+        multiplier= props.cold_friction_mult
+    elif weather_condition =="light_rain":
+        multiplier= props.light_rain_friction_mult
+    elif weather_condition =="heavy_rain":
+        multiplier= props.heavy_rain_friction_mult
+    else:
+        raise ValueError("Unsupported weather condition: "+ str(weather_condition))
+    
+    return base * multiplier
+
+def compute_safe_corner_speed(self, radius_m)
+    return math.sqrt(self.tyre_friction*9.8*radius_m)+self.level.car.crawl_const
+
+def plan_straight(self, straight_length_m, entry_speed_mps, required_exit_speed_mps):
+    max_speed= self.level.car.max_speed
+    accel_dist_to_max = self._acceleraation_distance(entry_speed_mps,max_speed,self.effective_accel,)
+    brake_dist=self._braking_distance(max_speed,required_exit_speed_mps,self.effective_brake,)
+
+    
 

e659f583e4f2ab1f0915609162a9f3dc64514805
traceyomphile <omphiletracey@gmail.com>
2026-04-18 11:45:21 +0200

    Defined the config file

--- Objects/Track.py
+++ Objects/Track.py
@@ -1,6 +1,12 @@
 # Track
 
 class Track:
+    degration_types = {
+        "K_STRAIGHT": 0.0000166,
+        "K_BRAKING": 0.0398,
+        "K_CORNER": 0.000265
+    }
+    
     def __init__(self, name, segments, id, type, length, radius, ):
         self.name = name
         self.segments = segments
@@ -8,5 +14,7 @@ class Track:
         self.type = type    
         self.length = length
         self.radius = radius
+
+    def 
 
         

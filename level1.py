88ddeecfbe9732690159fd5cb88442f8a5c957c7
Samkelo Mlotshwa <samkelomlotshwa0414@gmail.com>
2026-04-18 13:34:18 +0200

    another code

--- level1.py
+++ level1.py
@@ -22,6 +22,34 @@ class LevelData:
         track_data= data["track"]
         tyres_data= data["tyres"]
         weather_data= data["weather"]
-        
+
+class StraightPlan:
+    def __init__(self, target_mps,brake_start):
+        self.target_mps=target_mps
+        self.brake_start=brake_start
+
+class level1:
+    def __init__(self, level):
+        self.level=level
+        self.start_weather=self .start._get_start_weather()
+        self.effective_accel=self.car.acc* self.start_weather.acc_mult
+        self.effective_brake=self.level.car.brake * self.start_weather.dec_mult  
+        self.init_tyre_id, self_init_compound =self.choose_init_tyre()
+        self.tyre_friction= self.compute_tyre_friction(
+            self.init_compoud,
+            self.start_weather.condition,
+        )
+        self.corner_safe_speeds= self. _compute_corner_safe_speed ()
+    
+    def solve(self):
+        laps =[]
+        current_speed =0.0
+        for lap_number in range(1, self.level.race.laps +1):
+            lap_segments, current_speed = self._build_lap_segment(current_speed)
+            laps.append({
+                "lap": lap_number,
+                "segments":lap_segments,
+                "pit":{"enter":false},
+            })
         
 
